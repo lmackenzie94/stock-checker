486760ca28a9c15f2270034154c3b175f7ab3b85
Luke MacKenzie <mackenzieluke94@gmail.com>
2025-04-29 12:03:26 -0400

    improvements

---
+++ README.md
@@ -0,0 +1,32 @@
+# Black Toe Stock Checker
+
+A script to check the stock of Black Toe Running products.
+
+## Setup (on AWS EC2)
+
+- SSH into the EC2 instance (`ssh -i "aws-ec2-lukes-macbook.pem" [instance Public DNS]`)
+- update packages: `sudo apt update`
+- upgrade packages: `sudo apt upgrade -y`
+- clone the repo: `git clone https://github.com/luke-s-macbook/stock-checker.git`
+- `cd stock-checker`
+- install dependencies: `npm install`
+- add `.env` file
+- install Chromium browser (`sudo apt install -y chromium-browser`)
+- install Puppeteer dependencies (see list [here](https://www.browserless.io/blog/puppeteer-on-aws-ec2))
+- start the script with PM2: `pm2 start index.mjs --interpreter node --name "stock-checker"`
+- save the PM2 config: `pm2 save`
+- configure PM2 to start on boot: `pm2 startup`
+
+### Helpful PM2 commands
+
+- `pm2 list`
+- `pm2 logs stock-checker`
+- `pm2 stop stock-checker`
+- `pm2 delete stock-checker`
+
+## Deployment (to AWS EC2)
+
+- `ssh -i "aws-ec2-lukes-macbook.pem" [instance Public DNS]`
+- `cd stock-checker`
+- `git pull`
+- `pm2 restart stock-checker`
--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 import twilio from 'twilio';
 import cron from 'node-cron';
+import 'dotenv/config'
 
 // Products JSON
 import products from './products.json' with { type: 'json' };
@@ -15,14 +16,21 @@ const client = new twilio(accountSid, authToken);
 
 async function checkProduct(product) {
   const { name, url, size } = product;
-  const browser = await puppeteer.launch({ headless: 'new' });
+
+  const browser = await puppeteer.launch({
+    // executablePath: '/usr/bin/chromium-browser', <-- when running on EC2
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+ 
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36');
   await page.goto(url, { waitUntil: 'domcontentloaded' });
 
   const sizeSelector = `input[type="radio"][value="${size}"]`;
 
   await page.waitForSelector(sizeSelector, {
-    timeout: 10000
+    timeout: 30000
   });
 
   const isOutOfStock = await page.$eval(sizeSelector, el =>
@@ -51,9 +59,9 @@ async function sendSMSNotification(summary) {
   }
 }
 
-// Run check at 9am and 9pm EST
+// Run check at 8am, noon, and 8pm EST
 cron.schedule(
-  '0 9,21 * * *',
+  '0 8,12,20 * * *',
   () => {
     console.log(`⏰ Running check at ${new Date().toLocaleString()}`);
     checkStock();
